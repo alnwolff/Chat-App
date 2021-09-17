565dd9989fd42549fa465946bf561a03001e7a0f
Alan Sanchez <alnwolff@gmail.com>
2021-09-17 19:44:25 +0200

    create MessageList and SendMessageForm components

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MessageList from './MessageList';
+import SendMessageForm from './SendMessageForm';
 
 export default class ChatScreen extends Component {
+
+    state = {
+        messages: []
+    }
+
     render() {
         return (
             <div>
-                
+                <MessageList messages={this.state.messages}/>
+
+                <SendMessageForm />
             </div>
         )
     }
