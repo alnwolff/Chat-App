e4bb0155e592251f785a94b2e70ea3cc9d4b4125
Alan Sanchez <alnwolff@gmail.com>
2021-09-17 23:14:42 +0200

    style ChatScreen and add date info on messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,19 +1,44 @@
 import React, { Component } from 'react'
 
 export default class MessageList extends Component {
+
+    timeConverter = UNIX_timestamp => {
+        var a = new Date(UNIX_timestamp);
+        var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        var year = a.getFullYear();
+        var month = months[a.getMonth()];
+        var date = a.getDate();
+        var hour = a.getHours();
+        var min = (a.getMinutes()<10?'0':'') + a.getMinutes()
+        var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+        return time;
+    }
+
     render() {
         return (
-            <ul>
-               {this.props.messages.map(message => {
-                   return (
-                       <li>
-                           <div>
-                               <p>{message.author}</p>
-                               <p>{message.message}</p>
-                           </div>
-                       </li>
-                   )
-               })} 
+            <ul className='message-box'>
+                {this.props.messages.map((message) => {
+                    return (
+                        <li key={message._id}>
+                            {message.author === this.props.user ? (
+                                <div className='user-message'>
+                                    <div>
+                                        <p>{message.message}</p>
+                                        <p className='date'>{this.timeConverter(message.timestamp)}</p>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className='others-message' key={message._id}>
+                                <div>
+                                    <p id='author'>{message.author}</p>
+                                    <p>{message.message}</p>
+                                    <p className='date'>{this.timeConverter(message.timestamp)}</p>
+                                </div>
+                                </div>
+                            )}
+                        </li>
+                    )
+                })}
             </ul>
         )
     }
